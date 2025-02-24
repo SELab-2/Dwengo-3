8172f54be96b41bfc8145ab6894411864efe5c17
Martijn Heeren <martijn.heeren7@gmail.com>
2025-02-24 10:32:53 +0100

    Major changes to schema.prisma (#41)
    * minor changes to schema.prisma
    
    * first look at semi-complete database schema
    
    * migration of dev db
    
    * fix students and groups relation
    
    ---------
    
    Co-authored-by: Sviatoslav Harasymchuk <sviatoslav.harasymchuk@ugent.be>

---
+++ prisma/migrations/20250220212222_first_migration/migration.sql
@@ -0,0 +1,339 @@
+/*
+  Warnings:
+
+  - You are about to drop the `learning_objects` table. If the table is not empty, all the data it contains will be lost.
+  - You are about to drop the `learning_objects_keyword` table. If the table is not empty, all the data it contains will be lost.
+  - You are about to drop the `learning_path_nodes` table. If the table is not empty, all the data it contains will be lost.
+  - You are about to drop the `learning_path_transitions` table. If the table is not empty, all the data it contains will be lost.
+  - You are about to drop the `learning_paths` table. If the table is not empty, all the data it contains will be lost.
+
+*/
+-- CreateEnum
+CREATE TYPE "ContentTypeEnum" AS ENUM ('text/plain', 'text/markdown', 'image/image-block', 'image/image', 'audio/mpeg', 'application/pdf', 'extern', 'blockly');
+
+-- CreateEnum
+CREATE TYPE "ClassRole" AS ENUM ('TEACHER', 'STUDENT');
+
+-- CreateEnum
+CREATE TYPE "SubmissionType" AS ENUM ('MULTIPLE_CHOICE', 'FILE');
+
+-- DropForeignKey
+ALTER TABLE "learning_objects_keyword" DROP CONSTRAINT "learning_objects_keyword_lo_id_fkey";
+
+-- DropForeignKey
+ALTER TABLE "learning_path_nodes" DROP CONSTRAINT "learning_path_nodes_lo_hruid_fkey";
+
+-- DropForeignKey
+ALTER TABLE "learning_path_nodes" DROP CONSTRAINT "learning_path_nodes_lp_id_fkey";
+
+-- DropForeignKey
+ALTER TABLE "learning_path_transitions" DROP CONSTRAINT "learning_path_transitions_from_node_id_fkey";
+
+-- DropForeignKey
+ALTER TABLE "learning_path_transitions" DROP CONSTRAINT "learning_path_transitions_to_lo_hruid_fkey";
+
+-- DropTable
+DROP TABLE "learning_objects";
+
+-- DropTable
+DROP TABLE "learning_objects_keyword";
+
+-- DropTable
+DROP TABLE "learning_path_nodes";
+
+-- DropTable
+DROP TABLE "learning_path_transitions";
+
+-- DropTable
+DROP TABLE "learning_paths";
+
+-- DropEnum
+DROP TYPE "content_type_enum";
+
+-- CreateTable
+CREATE TABLE "LearningObject" (
+    "id" TEXT NOT NULL,
+    "hruid" TEXT NOT NULL,
+    "uuid" TEXT NOT NULL,
+    "version" INTEGER NOT NULL,
+    "language" TEXT NOT NULL,
+    "title" TEXT NOT NULL,
+    "description" TEXT,
+    "contentType" "ContentTypeEnum",
+    "targetAges" INTEGER[],
+    "teacherExclusive" BOOLEAN NOT NULL DEFAULT false,
+    "skosConcepts" TEXT[],
+    "educationalGoals" JSON[],
+    "copyright" TEXT,
+    "licence" TEXT,
+    "difficulty" DECIMAL,
+    "estimatedTime" DECIMAL,
+    "returnValue" JSON,
+    "available" BOOLEAN NOT NULL DEFAULT true,
+    "createdAt" TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    "content" TEXT NOT NULL,
+    "multipleChoice" JSON,
+    "canUploadSubmission" BOOLEAN NOT NULL DEFAULT false,
+
+    CONSTRAINT "LearningObject_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "LearningObjectKeyword" (
+    "loId" TEXT NOT NULL,
+    "keyword" TEXT NOT NULL,
+
+    CONSTRAINT "LearningObjectKeyword_pkey" PRIMARY KEY ("loId","keyword")
+);
+
+-- CreateTable
+CREATE TABLE "LearningPathNode" (
+    "id" TEXT NOT NULL,
+    "lpId" TEXT NOT NULL,
+    "loId" TEXT NOT NULL,
+    "instruction" TEXT,
+    "startNode" BOOLEAN NOT NULL DEFAULT false,
+
+    CONSTRAINT "LearningPathNode_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "LearningNodeTransition" (
+    "id" TEXT NOT NULL,
+    "fromNodeId" TEXT NOT NULL,
+    "nextNodeId" TEXT NOT NULL,
+    "condition" TEXT,
+
+    CONSTRAINT "LearningNodeTransition_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "LearningPath" (
+    "id" TEXT NOT NULL,
+    "hruid" TEXT NOT NULL,
+    "language" TEXT NOT NULL,
+    "title" TEXT NOT NULL,
+    "description" TEXT,
+    "image" TEXT,
+    "createdAt" TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+
+    CONSTRAINT "LearningPath_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "User" (
+    "id" TEXT NOT NULL,
+    "name" TEXT NOT NULL,
+    "surname" TEXT NOT NULL,
+    "role" "ClassRole" NOT NULL,
+
+    CONSTRAINT "User_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "Student" (
+    "id" TEXT NOT NULL,
+    "userId" TEXT NOT NULL,
+
+    CONSTRAINT "Student_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "Teacher" (
+    "id" TEXT NOT NULL,
+    "userId" TEXT NOT NULL,
+
+    CONSTRAINT "Teacher_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "Class" (
+    "id" TEXT NOT NULL,
+
+    CONSTRAINT "Class_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "ClassJoinRequest" (
+    "id" TEXT NOT NULL,
+    "classId" TEXT NOT NULL,
+    "userId" TEXT NOT NULL,
+
+    CONSTRAINT "ClassJoinRequest_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "Group" (
+    "id" TEXT NOT NULL,
+    "nodeId" TEXT NOT NULL,
+    "assignmentId" TEXT NOT NULL,
+
+    CONSTRAINT "Group_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "Assignment" (
+    "id" TEXT NOT NULL,
+    "lpId" TEXT NOT NULL,
+    "teacherId" TEXT NOT NULL,
+    "classId" TEXT NOT NULL,
+
+    CONSTRAINT "Assignment_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "AssignmentSubmission" (
+    "id" TEXT NOT NULL,
+    "groupId" TEXT NOT NULL,
+    "nodeId" TEXT NOT NULL,
+    "submissionType" "SubmissionType" NOT NULL,
+    "fileOriginalName" TEXT,
+    "fileName" TEXT,
+    "multipleChoiceAnswer" INTEGER,
+
+    CONSTRAINT "AssignmentSubmission_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "Chat" (
+    "id" TEXT NOT NULL,
+    "groupId" TEXT NOT NULL,
+
+    CONSTRAINT "Chat_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "Message" (
+    "id" SERIAL NOT NULL,
+    "content" TEXT NOT NULL,
+    "senderId" TEXT NOT NULL,
+    "chatId" TEXT NOT NULL,
+    "createdAt" TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+
+    CONSTRAINT "Message_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "_ClassToStudent" (
+    "A" TEXT NOT NULL,
+    "B" TEXT NOT NULL,
+
+    CONSTRAINT "_ClassToStudent_AB_pkey" PRIMARY KEY ("A","B")
+);
+
+-- CreateTable
+CREATE TABLE "_ClassToTeacher" (
+    "A" TEXT NOT NULL,
+    "B" TEXT NOT NULL,
+
+    CONSTRAINT "_ClassToTeacher_AB_pkey" PRIMARY KEY ("A","B")
+);
+
+-- CreateTable
+CREATE TABLE "_ChatToUser" (
+    "A" TEXT NOT NULL,
+    "B" TEXT NOT NULL,
+
+    CONSTRAINT "_ChatToUser_AB_pkey" PRIMARY KEY ("A","B")
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "LearningObject_hruid_version_language_key" ON "LearningObject"("hruid", "version", "language");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "Student_userId_key" ON "Student"("userId");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "Teacher_userId_key" ON "Teacher"("userId");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "AssignmentSubmission_groupId_nodeId_key" ON "AssignmentSubmission"("groupId", "nodeId");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "Chat_groupId_key" ON "Chat"("groupId");
+
+-- CreateIndex
+CREATE INDEX "_ClassToStudent_B_index" ON "_ClassToStudent"("B");
+
+-- CreateIndex
+CREATE INDEX "_ClassToTeacher_B_index" ON "_ClassToTeacher"("B");
+
+-- CreateIndex
+CREATE INDEX "_ChatToUser_B_index" ON "_ChatToUser"("B");
+
+-- AddForeignKey
+ALTER TABLE "LearningObjectKeyword" ADD CONSTRAINT "LearningObjectKeyword_loId_fkey" FOREIGN KEY ("loId") REFERENCES "LearningObject"("id") ON DELETE NO ACTION ON UPDATE NO ACTION;
+
+-- AddForeignKey
+ALTER TABLE "LearningPathNode" ADD CONSTRAINT "LearningPathNode_loId_fkey" FOREIGN KEY ("loId") REFERENCES "LearningObject"("id") ON DELETE NO ACTION ON UPDATE NO ACTION;
+
+-- AddForeignKey
+ALTER TABLE "LearningPathNode" ADD CONSTRAINT "LearningPathNode_lpId_fkey" FOREIGN KEY ("lpId") REFERENCES "LearningPath"("id") ON DELETE NO ACTION ON UPDATE NO ACTION;
+
+-- AddForeignKey
+ALTER TABLE "LearningNodeTransition" ADD CONSTRAINT "LearningNodeTransition_nextNodeId_fkey" FOREIGN KEY ("nextNodeId") REFERENCES "LearningPathNode"("id") ON DELETE NO ACTION ON UPDATE NO ACTION;
+
+-- AddForeignKey
+ALTER TABLE "LearningNodeTransition" ADD CONSTRAINT "LearningNodeTransition_fromNodeId_fkey" FOREIGN KEY ("fromNodeId") REFERENCES "LearningPathNode"("id") ON DELETE NO ACTION ON UPDATE NO ACTION;
+
+-- AddForeignKey
+ALTER TABLE "Student" ADD CONSTRAINT "Student_userId_fkey" FOREIGN KEY ("userId") REFERENCES "User"("id") ON DELETE NO ACTION ON UPDATE NO ACTION;
+
+-- AddForeignKey
+ALTER TABLE "Teacher" ADD CONSTRAINT "Teacher_userId_fkey" FOREIGN KEY ("userId") REFERENCES "User"("id") ON DELETE NO ACTION ON UPDATE NO ACTION;
+
+-- AddForeignKey
+ALTER TABLE "ClassJoinRequest" ADD CONSTRAINT "ClassJoinRequest_userId_fkey" FOREIGN KEY ("userId") REFERENCES "User"("id") ON DELETE NO ACTION ON UPDATE NO ACTION;
+
+-- AddForeignKey
+ALTER TABLE "ClassJoinRequest" ADD CONSTRAINT "ClassJoinRequest_classId_fkey" FOREIGN KEY ("classId") REFERENCES "Class"("id") ON DELETE NO ACTION ON UPDATE NO ACTION;
+
+-- AddForeignKey
+ALTER TABLE "Group" ADD CONSTRAINT "Group_nodeId_fkey" FOREIGN KEY ("nodeId") REFERENCES "LearningPathNode"("id") ON DELETE NO ACTION ON UPDATE NO ACTION;
+
+-- AddForeignKey
+ALTER TABLE "Group" ADD CONSTRAINT "Group_assignmentId_fkey" FOREIGN KEY ("assignmentId") REFERENCES "Assignment"("id") ON DELETE NO ACTION ON UPDATE NO ACTION;
+
+-- AddForeignKey
+ALTER TABLE "Assignment" ADD CONSTRAINT "Assignment_classId_fkey" FOREIGN KEY ("classId") REFERENCES "Class"("id") ON DELETE NO ACTION ON UPDATE NO ACTION;
+
+-- AddForeignKey
+ALTER TABLE "Assignment" ADD CONSTRAINT "Assignment_teacherId_fkey" FOREIGN KEY ("teacherId") REFERENCES "Teacher"("id") ON DELETE NO ACTION ON UPDATE NO ACTION;
+
+-- AddForeignKey
+ALTER TABLE "Assignment" ADD CONSTRAINT "Assignment_lpId_fkey" FOREIGN KEY ("lpId") REFERENCES "LearningPath"("id") ON DELETE NO ACTION ON UPDATE NO ACTION;
+
+-- AddForeignKey
+ALTER TABLE "AssignmentSubmission" ADD CONSTRAINT "AssignmentSubmission_nodeId_fkey" FOREIGN KEY ("nodeId") REFERENCES "LearningPathNode"("id") ON DELETE NO ACTION ON UPDATE NO ACTION;
+
+-- AddForeignKey
+ALTER TABLE "AssignmentSubmission" ADD CONSTRAINT "AssignmentSubmission_groupId_fkey" FOREIGN KEY ("groupId") REFERENCES "Group"("id") ON DELETE NO ACTION ON UPDATE NO ACTION;
+
+-- AddForeignKey
+ALTER TABLE "Chat" ADD CONSTRAINT "Chat_groupId_fkey" FOREIGN KEY ("groupId") REFERENCES "Group"("id") ON DELETE NO ACTION ON UPDATE NO ACTION;
+
+-- AddForeignKey
+ALTER TABLE "Message" ADD CONSTRAINT "Message_senderId_fkey" FOREIGN KEY ("senderId") REFERENCES "User"("id") ON DELETE NO ACTION ON UPDATE NO ACTION;
+
+-- AddForeignKey
+ALTER TABLE "Message" ADD CONSTRAINT "Message_chatId_fkey" FOREIGN KEY ("chatId") REFERENCES "Chat"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "_ClassToStudent" ADD CONSTRAINT "_ClassToStudent_A_fkey" FOREIGN KEY ("A") REFERENCES "Class"("id") ON DELETE CASCADE ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "_ClassToStudent" ADD CONSTRAINT "_ClassToStudent_B_fkey" FOREIGN KEY ("B") REFERENCES "Student"("id") ON DELETE CASCADE ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "_ClassToTeacher" ADD CONSTRAINT "_ClassToTeacher_A_fkey" FOREIGN KEY ("A") REFERENCES "Class"("id") ON DELETE CASCADE ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "_ClassToTeacher" ADD CONSTRAINT "_ClassToTeacher_B_fkey" FOREIGN KEY ("B") REFERENCES "Teacher"("id") ON DELETE CASCADE ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "_ChatToUser" ADD CONSTRAINT "_ChatToUser_A_fkey" FOREIGN KEY ("A") REFERENCES "Chat"("id") ON DELETE CASCADE ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "_ChatToUser" ADD CONSTRAINT "_ChatToUser_B_fkey" FOREIGN KEY ("B") REFERENCES "User"("id") ON DELETE CASCADE ON UPDATE CASCADE;
