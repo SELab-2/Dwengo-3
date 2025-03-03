1266c01609edfd2159bc4a30fd7dd12d31488332
Sviatoslav Harasymchuk <sviatoslav.harasymchuk@gmail.com>
2025-03-03 14:00:13 +0100

    update scripts

--- db/schemaspy/generate_schema.sh
+++ db/schemaspy/generate_schema.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
 
+set -uo pipefail -o noclobber
+cd "$(dirname "$0")" || exit
+
 sudo docker pull schemaspy/schemaspy
-sudo docker run --rm -v "$PWD/result:/output" -v "./schemaspy.properties:/schemaspy.properties" schemaspy/schemaspy
+sudo docker run --rm -v "./result:/output" -v "./schemaspy.properties:/schemaspy.properties" schemaspy/schemaspy
--- db/schemaspy/publish_schema.sh
+++ db/schemaspy/publish_schema.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
 
-sudo cp -r result/* /var/www/html
+set -uo pipefail -o noclobber
+cd "$(dirname "$0")" || exit
+
+sudo cp -r result /var/www/html
 sudo systemctl restart caddy
