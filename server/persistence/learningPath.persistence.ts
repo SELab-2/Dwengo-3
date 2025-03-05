592f6c08799cfc9cb6fbfab22bbe78abc425ff55
Jeffrey Debaere <jeffrey.debaere@ugent.be>
2025-03-05 12:21:53 +0100

    use prisma singleton instance

--- server/persistence/learningPath.persistence.ts
+++ server/persistence/learningPath.persistence.ts
@@ -1,85 +1,85 @@
-import { Prisma, PrismaClient } from "@prisma/client";
-import { LearningPathByFilterParams, LearningPathCreateParams, PaginationParams } from "../domain/types";
-
-const prisma = new PrismaClient();
+import { Prisma } from "@prisma/client";
+import { PrismaSingleton } from "./prismaSingleton";
+import {
+  LearningPathByFilterParams,
+  LearningPathCreateParams,
+  PaginationParams,
+} from "../domain/types";
 
 export class LearningPathPersistence {
-
-    public async getLearningPaths(
-        filters: LearningPathByFilterParams,
-        paginationParams: PaginationParams
-    ) {
-
-        const whereClause: Prisma.LearningPathWhereInput = {
-            AND: [
-                filters.keywords && filters.keywords.length > 0
-                    ? {
-                        learningPathNodes: {
-                            some: {
-                                learningObject: {
-                                    learningObjectsKeywords: {
-                                        some: {
-                                            // TODO is a separate table for keywords necessary?
-                                            keyword: {
-                                                in: filters.keywords, // Match any of the keywords
-                                                mode: Prisma.QueryMode.insensitive, // Case-insensitive search
-                                            },
-                                        },
-                                    },
-                                },
-                            },
+  public async getLearningPaths(
+    filters: LearningPathByFilterParams,
+    paginationParams: PaginationParams
+  ) {
+    const whereClause: Prisma.LearningPathWhereInput = {
+      AND: [
+        filters.keywords && filters.keywords.length > 0
+          ? {
+              learningPathNodes: {
+                some: {
+                  learningObject: {
+                    learningObjectsKeywords: {
+                      some: {
+                        // TODO is a separate table for keywords necessary?
+                        keyword: {
+                          in: filters.keywords, // Match any of the keywords
+                          mode: Prisma.QueryMode.insensitive, // Case-insensitive search
                         },
-                    }
-                    : {},
-                filters.age && filters.age.length > 0
-                    ? {
-                        learningPathNodes: {
-                            some: {
-                                learningObject: {
-                                    targetAges: {
-                                        hasSome: filters.age, // Match any of the target ages
-                                    },
-                                },
-                            },
-                        },
-                    }
-                    : {},
-
-                filters.id ? { id: filters.id } : {},
-            ].filter(Boolean), // Remove empty objects from the AND array
-        };
-
-
-        const [learningPaths, totalCount] = await prisma.$transaction([
-            prisma.learningPath.findMany({
-                where: whereClause,
-                include: {
-                    learningPathNodes: {
-                        include: {
-                            learningObject: true,
-                        },
+                      },
                     },
+                  },
                 },
-                skip: paginationParams.skip,
-                take: paginationParams.pageSize,
+              },
+            }
+          : {},
+        filters.age && filters.age.length > 0
+          ? {
+              learningPathNodes: {
+                some: {
+                  learningObject: {
+                    targetAges: {
+                      hasSome: filters.age, // Match any of the target ages
+                    },
+                  },
+                },
+              },
+            }
+          : {},
+
+        filters.id ? { id: filters.id } : {},
+      ].filter(Boolean), // Remove empty objects from the AND array
+    };
+
+    const [learningPaths, totalCount] =
+      await PrismaSingleton.instance.$transaction([
+        PrismaSingleton.instance.learningPath.findMany({
+          where: whereClause,
+          include: {
+            learningPathNodes: {
+              include: {
+                learningObject: true,
+              },
             },
-            ),
-            prisma.learningPath.count({
-                where: whereClause,
-            }),
-        ]);
+          },
+          skip: paginationParams.skip,
+          take: paginationParams.pageSize,
+        }),
+        PrismaSingleton.instance.learningPath.count({
+          where: whereClause,
+        }),
+      ]);
 
-        return {
-            data: learningPaths,
-            totalPages: Math.ceil(totalCount / paginationParams.pageSize),
-        }
-    }
+    return {
+      data: learningPaths,
+      totalPages: Math.ceil(totalCount / paginationParams.pageSize),
+    };
+  }
 
-    public async createLearningPath(data: LearningPathCreateParams) {
-        // create a learningPath without any connected nodes
-        const learningPath = await prisma.learningPath.create({
-            data: data,
-        });
-        return learningPath;
-    }
+  public async createLearningPath(data: LearningPathCreateParams) {
+    // create a learningPath without any connected nodes
+    const learningPath = await PrismaSingleton.instance.learningPath.create({
+      data: data,
+    });
+    return learningPath;
+  }
 }
--- server/persistence/learningPathNode.persistence.ts
+++ server/persistence/learningPathNode.persistence.ts
@@ -1,31 +1,28 @@
-import { PrismaClient } from "@prisma/client";
+import { PrismaSingleton } from "./prismaSingleton";
 import { LearningPathNodeCreateParams } from "../domain/types";
 
-const prisma = new PrismaClient();
-
-
 export class LearningPathNodePersistence {
-
-    public async createLearningPathNode(
-        learningPathNode: LearningPathNodeCreateParams,
-    ) {
-        // create a learningPathNode without transitions and connect it to the learningPath
-        const { lpId, loId, ...data } = learningPathNode;
-        const createdLearningPathNode = await prisma.learningPathNode.create({
-            data: {
-                ...data,
-                learningPath: {
-                    connect: {
-                        id: lpId
-                    },
-                },
-                learningObject: {
-                    connect: {
-                        id: loId
-                    },
-                },
+  public async createLearningPathNode(
+    learningPathNode: LearningPathNodeCreateParams
+  ) {
+    // create a learningPathNode without transitions and connect it to the learningPath
+    const { lpId, loId, ...data } = learningPathNode;
+    const createdLearningPathNode =
+      await PrismaSingleton.instance.learningPathNode.create({
+        data: {
+          ...data,
+          learningPath: {
+            connect: {
+              id: lpId,
             },
-        });
-        return createdLearningPathNode;
-    }
+          },
+          learningObject: {
+            connect: {
+              id: loId,
+            },
+          },
+        },
+      });
+    return createdLearningPathNode;
+  }
 }
