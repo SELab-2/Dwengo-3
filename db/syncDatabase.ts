bef729d7e9d5cb48d4bca56d97ebb1df2d914d4b
Sviatoslav Harasymchuk <sviatoslav.harasymchuk@gmail.com>
2025-03-06 21:05:53 +0100

    synchronization added, to execute run: npm run start:sync

---
+++ db/syncDatabase.ts
@@ -0,0 +1,223 @@
+import {ContentTypeEnum, PrismaClient} from '@prisma/client';
+import axios from 'axios';
+import cliProgress from 'cli-progress';
+
+
+const contentTypeMap: Map<string, ContentTypeEnum> = new Map();
+contentTypeMap.set("text/plain", ContentTypeEnum.TEXT_PLAIN);
+contentTypeMap.set("text/markdown", ContentTypeEnum.TEXT_MARKDOWN);
+contentTypeMap.set("image/image-block", ContentTypeEnum.IMAGE_IMAGE_BLOCK);
+contentTypeMap.set("image/image", ContentTypeEnum.IMAGE_IMAGE);
+contentTypeMap.set("audio/mpeg", ContentTypeEnum.AUDIO_MPEG);
+contentTypeMap.set("application/pdf", ContentTypeEnum.APPLICATION_PDF);
+contentTypeMap.set("extern", ContentTypeEnum.EXTERN);
+contentTypeMap.set("blockly", ContentTypeEnum.BLOCKLY);
+
+
+const API_URLS = {
+    learningObjectsMeta: 'https://dwengo.org/backend/api/learningObject/search?all=',
+    learningObjectsContentById: 'https://dwengo.org/backend/api/learningObject/getRaw', // append hruid, version, language
+    learningPaths: 'https://dwengo.org/backend/api/learningPath/search?all=',
+};
+
+async function fetchRemoteData(url: string) {
+    try {
+        const response = await axios.get(url);
+        return response.data;
+    } catch (error) {
+        console.error(`Failed to fetch data from ${url}`, error);
+        return [];
+    }
+}
+
+async function fullSyncLearningObjects(prisma: PrismaClient) {
+    const remoteObjects = await fetchRemoteData(API_URLS.learningObjectsMeta);
+
+    // Initialize the progress bar
+    const progressBar = new cliProgress.SingleBar({}, cliProgress.Presets.shades_classic);
+    progressBar.start(remoteObjects.length, 0);
+
+    for (const [index, remote] of remoteObjects.entries()) {
+        const rawContent = await fetchRemoteData(`${API_URLS.learningObjectsContentById}?hruid=${remote.hruid}&version=${remote.version}&language=${remote.language}`);
+
+        await prisma.learningObject.upsert({
+            where: {
+                hruid_version_language: {
+                    hruid: remote.hruid,
+                    version: remote.version,
+                    language: remote.language,
+                },
+            },
+            update: {
+                title: remote.title,
+                description: remote.description,
+                contentType: contentTypeMap.get(remote.content_type),
+                targetAges: remote.target_ages,
+                teacherExclusive: remote.teacher_exclusive,
+                skosConcepts: remote.skos_concepts,
+                educationalGoals: remote.educational_goals,
+                copyright: remote.copyright,
+                licence: remote.licence,
+                difficulty: remote.difficulty,
+                estimatedTime: remote.estimated_time,
+                returnValue: remote.return_value,
+                available: remote.available,
+                updatedAt: new Date(Date.now()),
+                content: rawContent,
+                keywords: remote.keywords,
+            },
+            create: {
+                id: remote._id,
+                hruid: remote.hruid,
+                uuid: remote.uuid,
+                version: remote.version,
+                language: remote.language,
+                title: remote.title,
+                description: remote.description,
+                contentType: contentTypeMap.get(remote.content_type),
+                targetAges: remote.target_ages,
+                teacherExclusive: remote.teacher_exclusive,
+                skosConcepts: remote.skos_concepts,
+                educationalGoals: remote.educational_goals,
+                copyright: remote.copyright,
+                licence: remote.licence,
+                difficulty: remote.difficulty,
+                estimatedTime: remote.estimated_time,
+                returnValue: remote.return_value,
+                available: remote.available,
+                content: rawContent,
+                keywords: remote.keywords,
+            },
+        });
+
+        // Update the progress bar
+        progressBar.update(index + 1);
+    }
+
+    // Stop the progress bar
+    progressBar.stop();
+}
+
+async function fullSyncLearningPaths(prisma: PrismaClient) {
+    const remotePaths = await fetchRemoteData(API_URLS.learningPaths);
+
+    // Initialize the progress bar
+    const progressBar = new cliProgress.SingleBar({}, cliProgress.Presets.shades_classic);
+    progressBar.start(remotePaths.length, 0);
+
+    for (const [index, path] of remotePaths.entries()) {
+        await prisma.learningPath.upsert({
+            where: {
+                hruid_language: {
+                    hruid: path.hruid,
+                    language: path.language,
+                },
+            },
+            update: {
+                title: path.title,
+                description: path.description,
+                image: path.image,
+                updatedAt: new Date(Date.now()),
+            },
+            create: {
+                id: path._id,
+                hruid: path.hruid,
+                language: path.language,
+                title: path.title,
+                description: path.description,
+                image: path.image,
+            },
+        });
+
+        for (const node of path.nodes) {
+            const learningObject = await prisma.learningObject.findUnique({
+                where: {
+                    hruid_version_language: {
+                        hruid: node.learningobject_hruid,
+                        version: node.version,
+                        language: node.language,
+                    }
+                }, select: {id: true}
+            });
+
+            await prisma.learningPathNode.upsert({
+                where: {id: node._id},
+                update: {
+                    learningPathId: path._id,
+                    learningObjectId: learningObject!.id,
+                    startNode: node.start_node,
+                    instruction: node.instruction,
+                },
+                create: {
+                    id: node._id,
+                    learningPathId: path._id,
+                    learningObjectId: learningObject!.id,
+                    startNode: node.start_node,
+                    instruction: node.instruction,
+                },
+            });
+        }
+        for (const [node_index, node] of path.nodes.entries()) {
+            for (const transition of node.transitions) {
+                const learningObject = await prisma.learningObject.findUnique({
+                    where: {
+                        hruid_version_language: {
+                            hruid: transition.next.hruid,
+                            version: transition.next.version,
+                            language: transition.next.language,
+                        },
+                    },
+                    include: {
+                        learningPathNodes: true,
+                    },
+                });
+
+                let toNode = await learningObject?.learningPathNodes.find(node => node.learningPathId === path._id);
+
+                if (!toNode) {
+                    // KANKER API geeft transitions naar nodes die niet bestaan
+                    // Ik los dit op door gewoon naar de volgende node te verwijzen 🤬
+                    toNode = path.nodes[node_index + 1 % path.nodes.length];
+                }
+
+                await prisma.learningNodeTransition.upsert({
+                    where: {id: transition._id},
+                    update: {
+                        fromNodeId: node._id,
+                        toNodeId: toNode!.id,
+                        condition: transition.default ? null : 'true==true',
+                    },
+                    create: {
+                        id: transition._id,
+                        fromNodeId: node._id,
+                        toNodeId: toNode!.id,
+                        condition: transition.default ? null : 'true==true',
+                    },
+                });
+            }
+        }
+        // Update the progress bar
+        progressBar.update(index + 1);
+    }
+
+    // Stop the progress bar
+    progressBar.stop();
+}
+
+async function syncDatabases(prisma: PrismaClient) {
+
+    try {
+        console.log('Starting LearningObjects synchronization...');
+        await fullSyncLearningObjects(prisma);
+        console.log('LearningObjects synchronization completed successfully!');
+
+        console.log('Starting LearningPaths synchronization...');
+        await fullSyncLearningPaths(prisma);
+        console.log('LearningPaths synchronization completed successfully!');
+
+    } catch (error) {
+        console.error('Error syncing databases:', error);
+    }
+}
+
+export {syncDatabases};
