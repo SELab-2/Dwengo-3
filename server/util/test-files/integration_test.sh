6d4873768d549146877b194d5c8bf802b6999acb
RicoDeRijcke <RicoDeRijcke@github.com>
2025-03-10 09:08:22 +0100

    docker test database

---
+++ server/util/test-files/integration_test.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+cd ./util/test-files
+source .env.test
+docker compose up -d 
+
+cd ../../../db
+npx prisma generate dev
+
+cd ../server
+vitest run -c ./util/test-files/vitest.config.integration.ts
+
+cd ./util/test-files
+docker compose down
--- server/util/test-files/vitest.config.integration.ts
+++ server/util/test-files/vitest.config.integration.ts
@@ -2,6 +2,6 @@ import { defineConfig } from "vitest/config";
 
 export default defineConfig({
   test: {
-    include: ["./domain/**/*.test.ts", "./routes/**/*.test.ts"],
+    include: ["./persistence/**/*.test.ts"],
   },
 });
