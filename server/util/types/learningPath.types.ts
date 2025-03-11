d7f4aeab544a8045815761dd92b894944eef1b10
martijnheeren <martijn.heeren7@gmail.com>
2025-03-11 18:19:35 +0100

    basic swagger documentation for empty learningPath

--- server/util/types/learningPath.types.ts
+++ server/util/types/learningPath.types.ts
@@ -1,26 +1,25 @@
 import { z } from "zod";
 
-
-
-
 export const LearningPathFilterSchema = z.object({
-    keywords: z.array(z.string()).optional(),
-    age: z.array(z.string())
-        .transform((val) => val.map(Number))
-        .optional(),
-    id: z.string().optional(),
+  keywords: z.array(z.string()).optional(),
+  age: z
+    .array(z.string())
+    .transform((val) => val.map(Number))
+    .optional(),
+  id: z.string().optional(),
 });
 
 export const LearningPathCreateSchema = z.object({
-    hruid: z.string(),
-    language: z.string(),
-    title: z.string(),
-    description: z.string().optional(),
-    image: z.string().optional(),
-    createdAt: z.coerce.date().optional(),
-    updatedAt: z.coerce.date().optional(),
+  hruid: z.string(),
+  language: z.string(),
+  title: z.string(),
+  description: z.string().optional(),
+  image: z.string().optional(),
+  createdAt: z.coerce.date().optional(),
+  updatedAt: z.coerce.date().optional(),
 });
 
-
-export type LearningPathByFilterParams = z.infer<typeof LearningPathFilterSchema>;
+export type LearningPathByFilterParams = z.infer<
+  typeof LearningPathFilterSchema
+>;
 export type LearningPathCreateParams = z.infer<typeof LearningPathCreateSchema>;
