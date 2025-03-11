84793595676d6aed7913ab0acb1f32f01f44b0a0
martijnheeren <martijn.heeren7@gmail.com>
2025-03-11 19:25:58 +0100

    learningPathNode swagger put endpoint

--- server/routes/learningPathNode.routes.ts
+++ server/routes/learningPathNode.routes.ts
@@ -22,6 +22,36 @@ export class LearningPathNodeController {
   };
 
   private initializeRoutes() {
+    /**
+     * @swagger
+     * /api/learningPathNode:
+     *   post:
+     *     security:
+     *       - cookieAuth: []
+     *     tags:
+     *       - LearningPathNode
+     *     summary: Create a new learning path node
+     *     description: Creates a new learning path node with the provided data.
+     *     requestBody:
+     *       required: true
+     *       content:
+     *         application/json:
+     *           schema:
+     *             $ref: '#/components/schemas/LearningPathNodeCreate'
+     *     responses:
+     *       201:
+     *         description: Learning path node created successfully.
+     *         content:
+     *           application/json:
+     *             schema:
+     *               $ref: '#/components/schemas/LearningPathNodeGet'
+     *       400:
+     *         description: Bad request due to invalid input.
+     *       401:
+     *         description: Unauthorized, user not authenticated.
+     *       500:
+     *         description: Internal server error.
+     */
     this.router.post("/", this.createLearningPathNode);
   }
 }
