7d22b34e2af3e2b46207fa1bd731942446ff6f33
Lukas Boon <lukas.boon@ugent.be>
2025-03-12 16:30:38 +0100

    swagger discussion and message

--- server/routes/discussion.routes.ts
+++ server/routes/discussion.routes.ts
@@ -12,7 +12,82 @@ export class DiscussionController {
     }
 
     private initializeRoutes() {
+        /**
+         * @swagger
+         * /api/discussion:
+         *   get:
+         *     security:
+         *       - cookieAuth: []
+         *     tags:
+         *       - Discussion
+         *     summary: Get list of discussions
+         *     description: >
+         *       Fetches a list of discussions filtered by optional query parameters:
+         *       discussion ID or group IDs.
+         *     parameters:
+         *       - name: id
+         *         in: query
+         *         description: Filter by discussion ID
+         *         required: false
+         *         schema:
+         *           type: string
+         *       - name: groupIds
+         *         in: query
+         *         description: Filter by group IDs (multiple values allowed)
+         *         required: false
+         *         schema:
+         *           type: array
+         *           items:
+         *             type: string
+         *     responses:
+         *       200:
+         *         description: List of discussions
+         *         content:
+         *           application/json:
+         *             schema:
+         *               type: array
+         *               items:
+         *                 $ref: '#/components/schemas/Discussion'
+         *       400:
+         *         description: Bad request due to invalid parameters
+         *       401:
+         *         description: Unauthorized, user not authenticated
+         *       500:
+         *         description: Internal server error
+         */
         this.router.get('/', this.getDiscussions.bind(this));
+        /**
+         * @swagger
+         * /api/discussion:
+         *   put:
+         *     security:
+         *       - cookieAuth: []
+         *     tags:
+         *       - Discussion
+         *     summary: Create a new discussion
+         *     description: Create a new discussion.
+         *     requestBody:
+         *       required: true
+         *       content:
+         *         application/json:
+         *           schema:
+         *             $ref: '#/components/schemas/DiscussionCreate'
+         *     responses:
+         *       200:
+         *         description: List of discussions
+         *         content:
+         *           application/json:
+         *             schema:
+         *               type: array
+         *               items:
+         *                 $ref: '#/components/schemas/Discussion'
+         *       400:
+         *         description: Bad request due to invalid parameters
+         *       401:
+         *         description: Unauthorized, user not authenticated
+         *       500:
+         *         description: Internal server error
+         */
         this.router.put('/', this.createDiscussion.bind(this));
     }
 
--- server/routes/message.routes.ts
+++ server/routes/message.routes.ts
@@ -12,9 +12,112 @@ export class MessageController {
     }
 
     private initializeRoutes() {
+        /**
+         * @swagger
+         * /api/message:
+         *   get:
+         *     security:
+         *       - cookieAuth: []
+         *     tags:
+         *       - Message
+         *     summary: Get list of messages
+         *     description: >
+         *       Fetches a list of messages filtered by optional query parameters:
+         *       keywords, age, or message ID.
+         *     parameters:
+         *       - name: id
+         *         in: query
+         *         description: Filter by message ID
+         *         required: false
+         *         schema:
+         *           type: string
+         *       - name: dicussionId
+         *         in: query
+         *         description: Filter by discussion ID
+         *         required: false
+         *         schema:
+         *           type: string
+         *     responses:
+         *       200:
+         *         description: A list of messages
+         *         content:
+         *           application/json:
+         *             schema:
+         *               type: array
+         *               items:
+         *                 $ref: '#/components/schemas/Message'
+         *       400:
+         *         description: Bad request due to invalid parameters
+         *       401:
+         *         description: Unauthorized, user not authenticated
+         *       500:
+         *         description: Internal server error
+         */
         this.router.get('/', this.getMessages.bind(this));
+        /**
+         * @swagger
+         * /api/message:
+         *   put:
+         *     security:
+         *       - cookieAuth: []
+         *     tags:
+         *       - Message
+         *     summary: Create a message
+         *     description: Create a new message
+         *     requestBody:
+         *       required: true
+         *       content:
+         *         application/json:
+         *           schema:
+         *             $ref: '#/components/schemas/MessageCreate'
+         *     responses:
+         *       200:
+         *         description: The created message
+         *         content:
+         *           application/json:
+         *             schema:
+         *               $ref: '#/components/schemas/Message'
+         *       400:
+         *         description: Bad request due to invalid parameters
+         *       401:
+         *         description: Unauthorized, user not authenticated
+         *       500:
+         *         description: Internal server error
+         */
         this.router.put('/', this.createMessage.bind(this));
         //this.router.patch('/', this.updateMessage.bind(this));
+
+        /**
+         * @swagger
+         * /api/message/{id}:
+         *   delete:
+         *     security:
+         *       - cookieAuth: []
+         *     tags:
+         *       - Message
+         *     summary: Delete a message
+         *     description: Delete a message by ID
+         *     parameters:
+         *       - name: id
+         *         in: path
+         *         description: ID of the message to delete
+         *         required: true
+         *         schema:
+         *           type: integer
+         *     responses:
+         *       200:
+         *         description: The deleted message
+         *         content:
+         *           application/json:
+         *             schema:
+         *               $ref: '#/components/schemas/Message'
+         *       400:
+         *         description: Bad request due to invalid parameters
+         *       401:
+         *         description: Unauthorized, user not authenticated
+         *       500:
+         *         description: Internal server error
+         */
         this.router.delete('/:id', this.deleteMessage.bind(this));
     }
 
