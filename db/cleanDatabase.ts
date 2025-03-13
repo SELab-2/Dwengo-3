36459c534764dccd308d70c7237c24605d639858
Sviatoslav Harasymchuk <sviatoslav.harasymchuk@gmail.com>
2025-03-13 13:50:11 +0100

    fix chat -> discussion

--- db/cleanDatabase.ts
+++ db/cleanDatabase.ts
@@ -1,35 +1,36 @@
-import {PrismaClient} from "@prisma/client";
+import { PrismaClient } from "@prisma/client";
 
-async function deleteRecords(prisma: PrismaClient, deleteAdditionalRecords: boolean = false) {
-    console.log('Deleting records...');
+async function deleteRecords(
+  prisma: PrismaClient,
+  deleteAdditionalRecords: boolean = false,
+) {
+  console.log("Deleting records...");
 
-    try {
-        await prisma.learningNodeTransition.deleteMany({});
-        await prisma.learningPathNode.deleteMany({});
-        await prisma.learningPath.deleteMany({});
-        await prisma.learningObject.deleteMany({});
-        console.log('Synced records deleted successfully!');
+  try {
+    await prisma.learningNodeTransition.deleteMany({});
+    await prisma.learningPathNode.deleteMany({});
+    await prisma.learningPath.deleteMany({});
+    await prisma.learningObject.deleteMany({});
+    console.log("Synced records deleted successfully!");
 
-
-        if (deleteAdditionalRecords) {
-            await prisma.assignmentSubmission.deleteMany({});
-            await prisma.assignment.deleteMany({});
-            await prisma.group.deleteMany({});
-            await prisma.classJoinRequest.deleteMany({});
-            await prisma.class.deleteMany({});
-            await prisma.teacher.deleteMany({});
-            await prisma.student.deleteMany({});
-            await prisma.message.deleteMany({});
-            await prisma.chat.deleteMany({});
-            await prisma.user.deleteMany({});
-            console.log('Additional records deleted successfully!');
-        }
-
-    } catch (error) {
-        console.error('Error deleting records:', error);
-    } finally {
-        await prisma.$disconnect();
+    if (deleteAdditionalRecords) {
+      await prisma.assignmentSubmission.deleteMany({});
+      await prisma.assignment.deleteMany({});
+      await prisma.group.deleteMany({});
+      await prisma.classJoinRequest.deleteMany({});
+      await prisma.class.deleteMany({});
+      await prisma.teacher.deleteMany({});
+      await prisma.student.deleteMany({});
+      await prisma.message.deleteMany({});
+      await prisma.discussion.deleteMany({});
+      await prisma.user.deleteMany({});
+      console.log("Additional records deleted successfully!");
     }
+  } catch (error) {
+    console.error("Error deleting records:", error);
+  } finally {
+    await prisma.$disconnect();
+  }
 }
 
-export {deleteRecords};
+export { deleteRecords };
