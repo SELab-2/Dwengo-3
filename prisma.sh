af5ef38c9d68e0633db0f5f65aa8c38d897f0ed7
Sviatoslav Harasymchuk <sviatoslav.harasymchuk@gmail.com>
2025-03-13 16:24:53 +0100

    fix script to copy the prisma module to server/node_modules folder

--- prisma.sh
+++ prisma.sh
@@ -2,11 +2,11 @@
 
 set -uo pipefail -o noclobber
 
-cd server || exit
-dotenv -e ../.env -- npx prisma generate --no-hints
+cd db && dotenv -e ../.env -- npx prisma generate --no-hints
 
 if (( $# > 1 )) && [[ "$1" == "migrate" ]]; then
-    cd ../db || exit
     dotenv -e ../.env -- npx prisma migrate "$2"
 fi
 
+cp -r node_modules/@prisma ../server/node_modules/
+cp -r node_modules/.prisma ../server/node_modules/
