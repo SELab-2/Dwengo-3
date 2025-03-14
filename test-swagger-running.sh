f034823840d640d302514cbae4bea0f4a997a5e7
Sviatoslav Harasymchuk <sviatoslav.harasymchuk@gmail.com>
2025-03-14 21:58:12 +0100

    quick fix path

--- test-swagger-running.sh
+++ test-swagger-running.sh
@@ -2,6 +2,6 @@
 
 set -uo pipefail -o noclobber
 
-if [ $(curl -o /dev/null localhost:3001/docs -s -w "%{http_code}\n") != "200" ]; then
+if [ $(curl -o /dev/null localhost:3001/docs/ -s -w "%{http_code}\n") != "200" ]; then
     exit 1
 fi
