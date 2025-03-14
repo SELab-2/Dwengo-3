9855c1741159fbe81dffb881ca2ef325818c12b7
Sviatoslav Harasymchuk <sviatoslav.harasymchuk@gmail.com>
2025-03-15 00:44:36 +0100

    Update main.yml and install script

--- install.sh
+++ install.sh
@@ -3,7 +3,8 @@
 set -uo pipefail -o noclobber
 
 if (( $# > 1 )) && [[ "$1" == "production" ]]; then
-  cd server && npm ci
+  cd client && npm ci
+  cd ../server && npm ci
   cd ../db && npm ci
 else
   cd client && npm install
