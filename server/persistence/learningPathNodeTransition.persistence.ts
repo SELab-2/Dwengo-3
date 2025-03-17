16536b093282b4c3026ce788b2b8ad7ee58e499b
Sviatoslav Harasymchuk <sviatoslav.harasymchuk@gmail.com>
2025-03-17 14:52:39 +0100

    learningPathNodeTransition fix

--- server/persistence/learningPathNodeTransition.persistence.ts
+++ server/persistence/learningPathNodeTransition.persistence.ts
@@ -7,22 +7,10 @@ export class LearningPathNodeTransitionPersistence {
     data: LearningPathNodeTransitionCreateParams,
   ) {
     // Create a transition and connect it from previous to next node
-
-    const { fromNodeId, toNodeId, ...transitionData } = data;
     const transition =
       await PrismaSingleton.instance.learningNodeTransition.create({
         data: {
-          ...transitionData,
-          fromNode: {
-            connect: {
-              id: fromNodeId,
-            },
-          },
-          toNode: {
-            connect: {
-              id: toNodeId,
-            },
-          },
+          ...data,
         },
       });
     return transition;
