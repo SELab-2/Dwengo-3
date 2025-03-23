77e62e7e72294e1eb0f22412c55189f8618a7244
martijnheeren <martijn.heeren7@gmail.com>
2025-03-23 10:24:21 +0100

    discussion swagger documentation

--- server/routes/discussion.routes.ts
+++ server/routes/discussion.routes.ts
@@ -13,8 +13,104 @@ export class DiscussionController {
   }
 
   private initializeRoutes() {
+    /**
+     * @swagger
+     * /api/discussion:
+     *   get:
+     *     security:
+     *       - cookieAuth: []
+     *     tags:
+     *       - Discussion
+     *     summary: Get list of discussions
+     *     description: Fetches a list of discussions filtered by groupIds
+     *     parameters:
+     *       - name: groupIds
+     *         in: query
+     *         description: Filter by group IDs (multiple values allowed)
+     *         required: false
+     *         schema:
+     *           type: array
+     *           items:
+     *             type: string
+     *     responses:
+     *       200:
+     *         description: Successfully fetched list of discussions
+     *         content:
+     *           application/json:
+     *             schema:
+     *               allOf:
+     *                 - $ref: '#/components/schemas/PaginatedResponse'
+     *                 - type: object
+     *                   properties:
+     *                     data:
+     *                       type: array
+     *                       items:
+     *                         $ref: '#/components/schemas/DiscussionShort'
+     *       400:
+     *         description: Bad request due to invalid parameters
+     *       401:
+     *         description: Unauthorized, user not authenticated
+     */
     this.router.get('/', this.getDiscussions.bind(this));
+    /**
+     * @swagger
+     * /api/discussion/{id}:
+     *   get:
+     *     security:
+     *       - cookieAuth: []
+     *     tags:
+     *       - Discussion
+     *     summary: Get a discussion by ID
+     *     description: Gets the content of a specific discussion selected by its UUID.
+     *     parameters:
+     *       - in: path
+     *         name: id
+     *         required: true
+     *         schema:
+     *           type: string
+     *           format: uuid
+     *         description: The UUID of the discussion to fetch
+     *     responses:
+     *       200:
+     *         description: Successfully fetched the discussion
+     *         content:
+     *           application/json:
+     *             schema:
+     *               $ref: '#/components/schemas/DiscussionDetail'
+     *       403:
+     *         description: Unauthorized, user not authenticated.
+     *       404:
+     *         description: Discussion not found.
+     */
     this.router.get('/:id', this.getDiscussionById.bind(this));
+    /**
+     * @swagger
+     * /api/discussion:
+     *   put:
+     *     security:
+     *       - cookieAuth: []
+     *     tags:
+     *       - Discussion
+     *     summary: Create a new discussion
+     *     description: Create a new discussion.
+     *     requestBody:
+     *       required: true
+     *       content:
+     *         application/json:
+     *           schema:
+     *             $ref: '#/components/schemas/DiscussionCreate'
+     *     responses:
+     *       200:
+     *         description: Discussion successfully created
+     *         content:
+     *           application/json:
+     *             schema:
+     *                 $ref: '#/components/schemas/DiscussionDetail'
+     *       400:
+     *         description: Bad request due to invalid parameters
+     *       401:
+     *         description: Unauthorized, user not authenticated
+     */
     this.router.put('/', this.createDiscussion.bind(this));
   }
 
