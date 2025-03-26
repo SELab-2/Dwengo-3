7a1a3fc8e988e8f149ce21f23f148a44d4c3d9eb
martijnheeren <martijn.heeren7@gmail.com>
2025-03-26 12:04:36 +0100

    BugFix: fix teacherId was compared with userId

--- server/util/coockie-checks/coockieChecks.util.ts
+++ server/util/coockie-checks/coockieChecks.util.ts
@@ -1,7 +1,8 @@
 import { Class, ClassRole } from '@prisma/client';
-import { ClassPersistence } from '../../persistence/class.persistence';
+
 import { Uuid } from '../types/assignment.types';
 import { ClassRoleEnum, UserEntity } from '../types/user.types';
+import { ClassPersistence } from '../../persistence/class.persistence';
 import { GroupPersistence } from '../../persistence/group.persistence';
 
 export const compareUserIdWithFilterId = async (
@@ -69,7 +70,7 @@ export const checkIfUserIsInGroup = async (
 
   if (user.role === ClassRole.TEACHER) {
     const isTeacherOfThisGroup = groupData.assignment.class.teachers.some(
-      (teacher) => teacher.id === user.id,
+      (teacher) => user.teacher && teacher.id === user.teacher.id,
     );
     if (!isTeacherOfThisGroup) {
       throw new Error("Can't fetch groups you're not a teacher of.");
@@ -77,7 +78,7 @@ export const checkIfUserIsInGroup = async (
   }
   if (user.role === ClassRole.STUDENT) {
     const isStudentOfThisGroup = groupData.students.some(
-      (student) => student.id === user.id,
+      (student) => user.student && student.id === user.student.id,
     );
     if (!isStudentOfThisGroup) {
       throw new Error("Can't fetch groups you're not a student of.");
