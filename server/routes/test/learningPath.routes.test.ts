f7f9cdfda2159270f82fb6c2f38aa9783268dad8
jasmaert <maertenjasper@gmail.com>
2025-03-27 18:17:43 +0100

    learningPath.routes.test

---
+++ server/routes/test/learningPath.routes.test.ts
@@ -0,0 +1,77 @@
+import request from 'supertest';
+import { describe, beforeEach, test, vi, expect } from 'vitest';
+import { app } from '../../app';
+import { mock } from 'node:test';
+
+// Domain mock
+const { mockLearningPathDomain } = vi.hoisted(() => {
+  return {
+    mockLearningPathDomain: {
+      getLearningPaths: vi.fn(),
+      getLearningPathById: vi.fn(),
+      createLearningPath: vi.fn(),
+    },
+  };
+});
+vi.mock('../../domain/learningPath.domain', () => {
+  return {
+    LearningPathDomain: vi.fn().mockImplementation(() => {
+      return mockLearningPathDomain;
+    }),
+  };
+});
+
+const route = '/api/learningPath';
+
+describe('learningPath routes test', () => {
+  beforeEach(() => {
+    vi.resetAllMocks();
+  });
+
+  describe('GET /learningPath', () => {
+    test('Responds on getLearningPaths', async () => {
+      const query = { keywords: ['programming', 'python'] };
+      const expected = { endpoint: 'getLearningPaths' };
+      mockLearningPathDomain.getLearningPaths.mockResolvedValue(expected);
+
+      await request(app).get(`${route}`).query(query).expect(200, expected);
+
+      expect(mockLearningPathDomain.getLearningPaths).toHaveBeenCalledWith(query);
+    });
+  });
+
+  describe('GET /learningPath/:id', () => {
+    test('Responds on getLearningPathById', async () => {
+      const id = '550e8400-e29b-41d4-a716-446655440000';
+      const expected = { endpoint: 'getLearningPathById' };
+      mockLearningPathDomain.getLearningPathById.mockResolvedValue(expected);
+
+      await request(app).get(`${route}/${id}`).expect(200, expected);
+
+      expect(mockLearningPathDomain.getLearningPathById).toHaveBeenCalledWith(id);
+    });
+  });
+
+  describe('PUT /learningPath', () => {
+    test('Responds on createLearningPath', async () => {
+      const body = {
+        hruid: 'intro-to-programming',
+        language: 'en',
+        title: 'Introduction to Programming',
+        description: 'A beginner-friendly learning path for programming.',
+        image: 'string',
+        createdAt: '2023-06-25T10:00:00Z',
+        updatedAt: '2023-06-25T10:00:00Z',
+      };
+      const expected = { endpoint: 'createLearningPath' };
+      mockLearningPathDomain.createLearningPath.mockResolvedValue(expected);
+
+      await request(app).put(`${route}`).send(body).expect(200, expected);
+
+      expect(mockLearningPathDomain.createLearningPath).toHaveBeenCalledWith(
+        body,
+        expect.any(Object),
+      );
+    });
+  });
+});
