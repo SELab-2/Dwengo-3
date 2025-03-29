919e89832c220b02ec4d60f3686b57daed5a75a8
Jona Reynaert <Jona.Reynaert@UGent.be>
2025-03-29 13:49:24 +0100

    profile page implemented

---
+++ client/src/components/NotLoggedIn.tsx
@@ -0,0 +1,29 @@
+import { Box, Button, Typography } from '@mui/material';
+import { MarginSize } from '../util/size';
+import { Login as LoginIcon } from '@mui/icons-material';
+import { useTranslation } from 'react-i18next';
+
+// TODO: this component may be deleted as soon as the routes are protected
+function NotLoggedIn() {
+  const { t } = useTranslation();
+
+  return (
+    <Box
+      sx={{
+        display: 'flex',
+        height: '100%',
+        flexDirection: 'column',
+        py: MarginSize.large,
+      }}
+    >
+      <Typography variant="h5" component="h1" gutterBottom align="center">
+        {t('notLoggedIn')}
+      </Typography>
+      <Button startIcon={<LoginIcon />} href="/login">
+        {t('login')}
+      </Button>
+    </Box>
+  );
+}
+
+export default NotLoggedIn;
