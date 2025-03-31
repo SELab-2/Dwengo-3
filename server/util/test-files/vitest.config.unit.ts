317912cc52339be753c49f1e47fe16f619e12b84
jasmaert <maertenjasper@gmail.com>
2025-03-31 11:09:44 +0200

    Error in vitest config

--- server/util/test-files/vitest.config.unit.ts
+++ server/util/test-files/vitest.config.unit.ts
@@ -2,7 +2,7 @@ import { defineConfig } from 'vitest/config';
 
 export default defineConfig({
   test: {
-    include: ['./routes/**/q*.test.ts'],
+    include: ['./routes/**/*.test.ts'],
     setupFiles: './util/test-files/vitest.setup.ts',
   },
 });
