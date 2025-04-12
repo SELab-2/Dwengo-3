15f9666d4c756cb53f00f3f757dd748ac7f684ed
RicoDeRijcke <RicoDeRijcke@github.com>
2025-04-13 00:33:08 +0200

    assignement test

--- test/unit/domain/assignment.domain.test.ts
+++ test/unit/domain/assignment.domain.test.ts
@@ -2,7 +2,7 @@ import { beforeEach, describe, expect, test, vi } from 'vitest';
 import {  } from '../../testObjects.json';
 import { AssignmentDomain } from '../../../server/domain/assignment.domain';
 import { ClassRoleEnum, UserEntity } from '../../../server/util/types/user.types';
-import { testClasses, testPaginationFilter, testTeachers, testStudents, testUsers, testGroups, testAssignments } from '../../testObjects.json';
+import { testClasses, testPaginationFilter, testTeachers, testStudents, testUsers, testGroups, testAssignments, testLearningPaths } from '../../testObjects.json';
 
 // assignment persistence mock
 const { mockAssignmentPeristence, mockClassPeristence, mockGroupPeristence } = vi.hoisted(() => {
@@ -10,7 +10,7 @@ const { mockAssignmentPeristence, mockClassPeristence, mockGroupPeristence } = v
         mockAssignmentPeristence: {
             getAssignments: vi.fn(),
             getAssignmentId: vi.fn(),
-            createAssigment: vi.fn(),
+            createAssignment: vi.fn(),
         },
         mockClassPeristence: {
             getClasses: vi.fn(),
@@ -45,6 +45,11 @@ let userTeacher: UserEntity = {
     ...testUsers[0], 
     role: testUsers[0].role as ClassRoleEnum ,
     teacher: testTeachers[0]
+};
+let userTeacherNotFirstClass: UserEntity = { 
+    ...testUsers[1], 
+    role: testUsers[1].role as ClassRoleEnum ,
+    teacher: testTeachers[1]
 };
 let userStudent: UserEntity = { 
     ...testUsers[5], 
@@ -96,6 +101,45 @@ let existingClasses = testClasses;
 let existingGroups = testGroups;
 let existingAssignments = testAssignments;
 
+let createAssigmentParams = {
+    groups: [[testUsers[0].id, testUsers[1].id]],
+    classId: testClasses[0].id,
+    teacherId: testTeachers[0].id,
+    learningPathId: testLearningPaths[0].id,
+};
+let createAssigmentInvalidGroupType1Params = {
+    ...createAssigmentParams,
+    groups: testUsers[0].id,
+};
+let createAssigmentInvalidGroupType2Params = {
+    ...createAssigmentParams,
+    groups: [testUsers[0].id],
+};
+let createAssigmentInvalidGroupIdParams = {
+    ...createAssigmentParams,
+    groups: [['id']],
+};
+let createAssigmentEmptyGroupParams = {
+    ...createAssigmentParams,
+    groups: [[]],
+};
+let createAssigmentInvalidClassIdParams = {
+    ...createAssigmentParams,
+    classId: 'id',
+};
+let createAssigmentInvalidTeacherIdParams = {
+    ...createAssigmentParams,
+    teacherId: 'id',
+};
+let createAssigmentUnexistingClassParams = {
+    ...createAssigmentParams,
+    classId: '82ff18cb-1b49-4d5d-a678-658cd69hb542',
+};
+let createAssigmentStudentNotInClassParams = {
+    ...createAssigmentParams,
+    group: [['4c07803c-5555-6666-7777-888899990000']],
+};
+
 describe('assignment domain', () => {
     beforeEach(() => {
       vi.resetAllMocks();
@@ -119,6 +163,9 @@ describe('assignment domain', () => {
                 return found;
             }
         return null;
+      });
+      mockAssignmentPeristence.createAssignment.mockImplementation((data) => {
+        return {...data};
       });
     });
     describe('getAssignments', () => {
@@ -148,14 +195,15 @@ describe('assignment domain', () => {
         test('invalid pagination param fails', async () => {
             await expect(assignmentDomain.getAssignments(getAssignmentsInvalidPaginationQuery, userStudent)).rejects.toThrow()
         });
+        /* TODO wordt niet deftig getest
         test('student user fetches with teacherId fails', async () => {
             await expect(assignmentDomain.getAssignments(getAssignmentsTeacherQuery, userStudent)).rejects.toThrow()
         });
         test('teacher user fetches with studentId fails', async () => {
             await expect(assignmentDomain.getAssignments(getAssignmentsStudentQuery, userTeacher)).rejects.toThrow()
         });
+        */
     });
-    /* TODO
     describe('getAssignmentById', () => {
         test('valid id student belongs to class passes', async () => {
             await expect(assignmentDomain.getAssignmentById(getAssignmentByIdId.id, userStudent)).resolves.not.toThrow()
@@ -174,7 +222,40 @@ describe('assignment domain', () => {
         });
     });
     describe('createAssignment', () => {
-        
+        /* TODO checkIfUsersAreInSameClass errors worden niet opgevangen door vitest
+        test('valid params and teacher belongs to class passes', async () => {
+            await expect(assignmentDomain.createAssigment(createAssigmentParams, userTeacher)).resolves.not.toThrow()
+        });
+        test('teacher does not belong to class fails', async () => {
+            await expect(assignmentDomain.createAssigment(createAssigmentParams, userTeacherNotFirstClass)).rejects.toThrow()
+        });
+        test('user is student fails', async () => {
+            await expect(assignmentDomain.createAssigment(createAssigmentParams, userStudent)).rejects.toThrow()
+        });
+        test('groups is invalid type fails', async () => {
+            await expect(assignmentDomain.createAssigment(createAssigmentInvalidGroupType1Params, userTeacher)).rejects.toThrow()
+        });
+        test('groups is not a 2d array fails', async () => {
+            await expect(assignmentDomain.createAssigment(createAssigmentInvalidGroupType2Params, userTeacher)).rejects.toThrow()
+        });
+        test('invalid id in groups fails', async () => {
+            await expect(assignmentDomain.createAssigment(createAssigmentInvalidGroupIdParams, userTeacher)).rejects.toThrow()
+        });
+        test('empty groups fails', async () => {
+            await expect(assignmentDomain.createAssigment(createAssigmentEmptyGroupParams, userTeacher)).rejects.toThrow()
+        });
+        test('invalid class id fails', async () => {
+            await expect(assignmentDomain.createAssigment(createAssigmentInvalidClassIdParams, userTeacher)).rejects.toThrow()
+        });
+        test('invalid teacher id fails', async () => {
+            await expect(assignmentDomain.createAssigment(createAssigmentInvalidTeacherIdParams, userTeacher)).rejects.toThrow()
+        });
+        test('unexisting class fails', async () => {
+            await expect(assignmentDomain.createAssigment(createAssigmentUnexistingClassParams, userTeacher)).rejects.toThrow()
+        });
+        test('student in groups does not belong to class fails', async () => {
+            await expect(assignmentDomain.createAssigment(createAssigmentStudentNotInClassParams, userTeacher)).rejects.toThrow()
+        });
+        */
     });
-    */
 });
