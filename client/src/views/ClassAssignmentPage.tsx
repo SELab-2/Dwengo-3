8d34b963d7b0eeb11daafdc71e8e81e890b9681f
martijnheeren <martijn.heeren7@gmail.com>
2025-04-15 10:49:58 +0200

    Refactor code for improved readability and maintainability. And line up mockdata with actual selects in database

--- client/src/views/ClassAssignmentPage.tsx
+++ client/src/views/ClassAssignmentPage.tsx
@@ -1,4 +1,16 @@
-import { Box, Button, LinearProgress, Paper, Table, TableBody, TableCell, TableContainer, TableHead, TableRow, Typography } from '@mui/material';
+import {
+  Box,
+  Button,
+  LinearProgress,
+  Paper,
+  Table,
+  TableBody,
+  TableCell,
+  TableContainer,
+  TableHead,
+  TableRow,
+  Typography,
+} from '@mui/material';
 import { useParams } from 'react-router-dom';
 import ClassNavigationBar from '../components/ClassNavigationBar';
 import { useTranslation } from 'react-i18next';
@@ -7,195 +19,211 @@ import CheckIcon from '@mui/icons-material/Check';
 import ClearIcon from '@mui/icons-material/Clear';
 import GroupListDialog from '../components/GroupListDialog';
 import { useState } from 'react';
+import { title } from 'process';
+import { StudentShort } from '../util/types/user.types';
+import { LearningPathShort } from '../util/types/learningPath.types';
 
 const assignment = {
-  name: "Opdracht 1",
+  name: 'Opdracht 1',
+  id: '1',
   teacher: {
-    name: "leerkracht",
-    surname: "1"
+    id: '1234',
+    name: 'leerkracht',
+    surname: '1',
   },
   deadline: new Date(),
   groups: [
-    { 
-      name: "Groep 1", 
-      progress: 10, 
-      submission: true, 
+    {
+      assignmentId: '1',
+      name: 'Groep 1',
+      progress: [0, 1, 2],
       students: [
-        { name: "Liam", surname: "Janssen" },
-        { name: "Emma", surname: "Vermeulen" }
-      ]
+        { name: 'Liam', surname: 'Janssen', id: '1' },
+        { name: 'Emma', surname: 'Vermeulen', id: '2' },
+      ],
     },
-    { 
-      name: "Groep 2", 
-      progress: 45, 
-      submission: false, 
+    {
+      assignmentId: '1',
+      name: 'Groep 2',
+      progress: [0, 1, 2],
       students: [
-        { name: "Noah", surname: "Peeters" },
-        { name: "Sophie", surname: "De Vries" }
-      ]
+        { name: 'Noah', surname: 'Peeters', id: '3' },
+        { name: 'Sophie', surname: 'De Vries', id: '4' },
+      ],
     },
-    { 
-      name: "Groep 3", 
-      progress: 75, 
-      submission: true, 
+    {
+      assignmentId: '1',
+      name: 'Groep 3',
+      progress: [0, 1, 2],
       students: [
-        { name: "Lucas", surname: "Maes" },
-        { name: "Olivia", surname: "Van den Berg" }
-      ]
+        { name: 'Lucas', surname: 'Maes', id: '5' },
+        { name: 'Olivia', surname: 'Van den Berg', id: '6' },
+      ],
     },
-    { 
-      name: "Groep 4", 
-      progress: 20, 
-      submission: false, 
+    {
+      assignmentId: '1',
+      name: 'Groep 4',
+      progress: [0, 1, 2],
       students: [
-        { name: "Daan", surname: "Hendriks" },
-        { name: "Julie", surname: "Smeets" }
-      ]
+        { name: 'Daan', surname: 'Hendriks', id: '7' },
+        { name: 'Julie', surname: 'Smeets', id: '8' },
+      ],
     },
-    { 
-      name: "Groep 5", 
-      progress: 90, 
-      submission: true, 
+    {
+      assignmentId: '1',
+      name: 'Groep 5',
+      progress: [0, 1],
       students: [
-        { name: "Finn", surname: "Willems" },
-        { name: "Lotte", surname: "Claes" }
-      ]
+        { name: 'Finn', surname: 'Willems', id: '9' },
+        { name: 'Lotte', surname: 'Claes', id: '10' },
+      ],
     },
-    { 
-      name: "Groep 6", 
-      progress: 50, 
-      submission: false, 
+    {
+      assignmentId: '1',
+      name: 'Groep 6',
+      progress: [0, 1, 2],
       students: [
-        { name: "Milan", surname: "Jacobs" },
-        { name: "Elise", surname: "Coppens" }
-      ]
+        { name: 'Milan', surname: 'Jacobs', id: '11' },
+        { name: 'Elise', surname: 'Coppens', id: '12' },
+      ],
     },
-    { 
-      name: "Groep 7", 
-      progress: 30, 
-      submission: true, 
+    {
+      assignmentId: '1',
+      name: 'Groep 7',
+      progress: [0, 1, 2],
       students: [
-        { name: "Ruben", surname: "Goossens" },
-        { name: "Laura", surname: "Baert" }
-      ]
+        { name: 'Ruben', surname: 'Goossens', id: '13' },
+        { name: 'Laura', surname: 'Baert', id: '14' },
+      ],
     },
-    { 
-      name: "Groep 8", 
-      progress: 60, 
-      submission: false, 
+    {
+      assignmentId: '1',
+      name: 'Groep 8',
+      progress: [0, 1, 2],
       students: [
-        { name: "Vince", surname: "Mertens" },
-        { name: "Sanne", surname: "Van Dijk" }
-      ]
+        { name: 'Vince', surname: 'Mertens', id: '15' },
+        { name: 'Sanne', surname: 'Van Dijk', id: '16' },
+      ],
     },
-    { 
-      name: "Groep 9", 
-      progress: 80, 
-      submission: true, 
+    {
+      assignmentId: '1',
+      name: 'Groep 9',
+      progress: [0, 1, 2],
       students: [
-        { name: "Jens", surname: "Lemmens" },
-        { name: "Eva", surname: "De Bruin" }
-      ]
+        { name: 'Jens', surname: 'Lemmens', id: '17' },
+        { name: 'Eva', surname: 'De Bruin', id: '18' },
+      ],
     },
-    { 
-      name: "Groep 10", 
-      progress: 5, 
-      submission: false, 
+    {
+      assignmentId: '1',
+      name: 'Groep 10',
+      progress: [0, 1, 2],
       students: [
-        { name: "Timo", surname: "Van Acker" },
-        { name: "Nina", surname: "Bosmans" }
-      ]
+        { name: 'Timo', surname: 'Van Acker', id: '19' },
+        { name: 'Nina', surname: 'Bosmans', id: '20' },
+      ],
     },
-    { 
-      name: "Groep 11", 
-      progress: 95, 
-      submission: true, 
-      students: [
-        { name: "Stijn", surname: "Segers" },
-        { name: "Jade", surname: "De Smet" }
-      ]
-    },
-    { 
-      name: "Groep 12", 
-      progress: 40, 
-      submission: false, 
-      students: [
-        { name: "Matthias", surname: "Vandersteen" },
-        { name: "Eline", surname: "Pauwels" }
-      ]
-    },
-    { 
-      name: "Groep 13", 
-      progress: 70, 
-      submission: true, 
-      students: [
-        { name: "Nathan", surname: "Hoogland" },
-        { name: "Zoë", surname: "De Ruyter" }
-      ]
-    },
-    { 
-      name: "Groep 14", 
-      progress: 15, 
-      submission: false, 
-      students: [
-        { name: "Tobias", surname: "Coenen" },
-        { name: "Fien", surname: "De Koninck" }
-      ]
-    },
-    { 
-      name: "Groep 15", 
-      progress: 85, 
-      submission: true, 
-      students: [
-        { name: "Wout", surname: "Dewulf" },
-        { name: "Hanne", surname: "Michiels" }
-      ]
-    }
-  ]
-}
+  ],
+  learningPath: {
+    id: '1',
+    title: 'Leerpad 1',
+    description: 'Dit is een beschrijving van leerpad 1.',
+    image: 'https://via.placeholder.com/150',
+    learningPathNodes: [
+      {
+        learningObject: {
+          targetAges: [6, 7],
+          keywords: ['Math', 'Science'],
+        },
+      },
+      {
+        learningObject: {
+          targetAges: [6, 7],
+          keywords: ['Math', 'Science'],
+        },
+      },
+      {
+        learningObject: {
+          targetAges: [6, 7],
+          keywords: ['Math', 'Science'],
+        },
+      },
+      {
+        learningObject: {
+          targetAges: [6, 7],
+          keywords: ['Math', 'Science'],
+        },
+      },
+      {
+        learningObject: {
+          targetAges: [6, 7],
+          keywords: ['Math', 'Science'],
+        },
+      },
+    ],
+  },
+};
+
+const classData = {
+  id: '1',
+  name: 'Klas - 6 AIT',
+  teachers: ['Marnie Garcia', 'Marvin Kline'],
+  // ...
+};
+
+const calculateProgress = (
+  progress: number[],
+  learningPath: any /* TODO add type when interface is correct*/,
+) => {
+  const total_nodes = learningPath.learningPathNodes.length;
+  // + 1 is added because of zero indexing
+  return ((Math.max(...progress) + 1) / total_nodes) * 100;
+};
 
 function ClassAssignmentPage() {
   const { id, a_id } = useParams<{ id: string; a_id: string }>();
   const { t } = useTranslation();
 
-  const [ students, setStudents] = useState<{ name: string, surname: string }[]>([]);
-  const [ open, setOpen] = useState<boolean>(false);
+  const [students, setStudents] = useState<StudentShort[]>([]);
+  const [open, setOpen] = useState<boolean>(false);
 
   return (
     <Box sx={{ minHeight: '100vh', p: 3 }}>
-      <ClassNavigationBar id={id!} className="Class Name" />
+      <ClassNavigationBar id={id!} className={classData.name} />
 
-      <Box sx={{ mx: "auto", width: "100%", maxWidth: { xs: "90%", sm: 1000}, p: 2 }}>
+      <Box sx={{ mx: 'auto', width: '100%', maxWidth: { xs: '90%', sm: 1000 }, p: 2 }}>
         <Typography variant="h3" gutterBottom>
           {assignment.name}
         </Typography>
         <Typography variant="h5" gutterBottom>
           {`${t('givenBy')}: ${assignment.teacher.name} ${assignment.teacher.surname}`}
         </Typography>
-        <DateTypography text={`${t('deadline')}: `} date={assignment.deadline}/>
+        <DateTypography text={`${t('deadline')}: `} date={assignment.deadline} />
 
-        <GroupListDialog students={students} open={open} onClose={() => setOpen(false)}/>
+        <GroupListDialog students={students} open={open} onClose={() => setOpen(false)} />
 
-        <TableContainer 
-          component={Paper} 
-          sx={{ 
-            borderRadius: 2, 
-            maxHeight: 450, 
-            overflow: "auto", 
+        <TableContainer
+          component={Paper}
+          sx={{
+            borderRadius: 2,
+            maxHeight: 450,
+            overflow: 'auto',
             boxShadow: 3,
-            mx: "auto",
-            width: "100%",
-            maxWidth: { xs: "90%", sm: 1000 },
-            overflowX: "auto",
+            mx: 'auto',
+            width: '100%',
+            maxWidth: { xs: '90%', sm: 1000 },
+            overflowX: 'auto',
           }}
         >
           <Table sx={{ minWidth: 600 }}>
             <TableHead>
-              <TableRow 
-                sx={{ 
-                  position: "sticky", top: 0, zIndex: 1, 
-                  backgroundColor: "white", 
-                  borderBottom: "2px solid #ddd" 
+              <TableRow
+                sx={{
+                  position: 'sticky',
+                  top: 0,
+                  zIndex: 1,
+                  backgroundColor: 'white',
+                  borderBottom: '2px solid #ddd',
                 }}
               >
                 <TableCell sx={{ minWidth: 180, width: '50%' }}>
@@ -204,10 +232,7 @@ function ClassAssignmentPage() {
                 <TableCell sx={{ minWidth: 200, width: '50%' }}>
                   <Typography variant="h6">{t('progression')}</Typography>
                 </TableCell>
-                <TableCell sx={{ minWidth: 200, width: '50%' }}>
-                  <Typography variant="h6">{t('submission')}</Typography>
-                </TableCell>
-                <TableCell/>
+                <TableCell />
               </TableRow>
             </TableHead>
 
@@ -215,45 +240,41 @@ function ClassAssignmentPage() {
               {assignment.groups.map((group, index) => (
                 <TableRow key={index}>
                   <TableCell>
-                    <Typography 
-                    sx={{ 
-                      cursor: "pointer", 
-                      fontSize: { xs: "14px", sm: "16px" } 
-                    }}
-                    onClick={() => {
-                      setStudents(group.students);
-                      setOpen(true)}
-                    }
+                    <Typography
+                      sx={{
+                        cursor: 'pointer',
+                        fontSize: { xs: '14px', sm: '16px' },
+                      }}
+                      onClick={() => {
+                        setStudents(group.students);
+                        setOpen(true);
+                      }}
                     >
                       {group.name}
                     </Typography>
                   </TableCell>
 
                   <TableCell>
-                    <LinearProgress 
-                      variant="determinate" 
-                      value={group.progress} 
-                      sx={{ 
-                        height: 8, 
+                    <LinearProgress
+                      variant="determinate"
+                      value={calculateProgress(group.progress, assignment.learningPath)}
+                      sx={{
+                        height: 8,
                         borderRadius: 5,
-                        minWidth: { xs: "80px", sm: "150px", md: "200px" } 
-                      }} 
+                        minWidth: { xs: '80px', sm: '150px', md: '200px' },
+                      }}
                     />
                   </TableCell>
 
-                  <TableCell >
-                    {group.submission ? <CheckIcon color="primary" /> : <ClearIcon color="error" />}
-                  </TableCell>
-
-                  <TableCell sx={{ textAlign: "center" }}>
-                    <Button 
-                      variant="contained" 
+                  <TableCell sx={{ textAlign: 'center' }}>
+                    <Button
+                      variant="contained"
                       sx={{
-                        fontSize: { xs: "12px", sm: "14px" }, 
-                        padding: { xs: "5px 10px", sm: "8px 16px" }, 
-                        minWidth: { xs: "60px", sm: "100px" }
+                        fontSize: { xs: '12px', sm: '14px' },
+                        padding: { xs: '5px 10px', sm: '8px 16px' },
+                        minWidth: { xs: '60px', sm: '100px' },
                       }}
-                      onClick={() => alert("TODO")}
+                      onClick={() => alert('TODO')}
                     >
                       {t('details')}
                     </Button>
