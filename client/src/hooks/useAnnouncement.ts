fd5db1d044e55262e8fc8200388aba83412b7e98
RicoDeRijcke <RicoDeRijcke@github.com>
2025-04-24 20:23:01 +0200

    populated hook

--- client/src/hooks/useAnnouncement.ts
+++ client/src/hooks/useAnnouncement.ts
@@ -1,5 +1,7 @@
+import { AnnouncementDetail } from '../util/interfaces/announcement.interfaces';
 import { useQuery } from '@tanstack/react-query';
 import { fetchAnnouncementById, fetchAnnouncements } from '../api/announcement';
+import { fetchNestedData } from '../api/util';
 
 /**
  * Fetches an announcement by its ID.
@@ -41,6 +43,43 @@ export function useAnnouncements(
       return await fetchAnnouncements(classId, teacherId, studentId, page, pageSize);
     },
     enabled: !!classId || !!teacherId || !!studentId,
+    refetchOnWindowFocus: false,
+  });
+}
+
+/**
+ * Fetches a list of announcements
+ *
+ * @param page - The pagenumber of the pagination you want to fetch
+ * @param pageSize - The number of items you want to fetch
+ * @param classId - The id of the class whose announcements are to be fetched
+ * @param teacherId - The id of the teacher whose announcements are to be fetched
+ * @param studentId - The id of the student whose announcements are to be fetched
+ * @returns A list of announcements
+ */
+export function useAnnouncementDetails(
+  classId?: string,
+  teacherId?: string,
+  studentId?: string,
+  page?: number,
+  pageSize?: number,
+) {
+  return useQuery({
+    queryKey: ['announcementDetails', classId, teacherId, studentId, page, pageSize],
+    queryFn: async () => {
+      const paginatedData = await fetchAnnouncements(classId, teacherId, studentId, page, pageSize);
+
+      const announcementDetails: AnnouncementDetail[] = await fetchNestedData(
+        paginatedData.data.map((announcementShort) => announcementShort.id),
+        (announcementId) => fetchAnnouncementById(announcementId),
+      );
+
+      return {
+        ...paginatedData,
+        data: announcementDetails, // Replace the data with populated classes
+      };
+    },
+    enabled: !!studentId || !!teacherId, // Enable the query only if studentId or teacherId is provided
     refetchOnWindowFocus: false,
   });
 }
