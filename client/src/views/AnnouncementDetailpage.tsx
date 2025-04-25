2215b0cb82fcf66586b2483e7623fd62601d9d42
Sviatoslav Harasymchuk <sviatoslav.harasymchuk@gmail.com>
2025-04-25 03:17:31 +0200

    fix back button and references

--- client/src/views/AnnouncementDetailpage.tsx
+++ client/src/views/AnnouncementDetailpage.tsx
@@ -9,38 +9,38 @@ import { AppRoutes } from '../util/app.routes.ts';
 import ClassNavigationBar from '../components/ClassNavigationBar.tsx';
 import { MarginSize } from '../util/size.ts';
 
-function AnnouncementDetailpage({ classId, className }: { classId: string; className: string }) {
+function AnnouncementDetailpage() {
   const { user } = useAuth(); // NEEDED TO CHECK IF USER IS TEACHER OR STUDENT for future edit features
   const { t } = useTranslation();
   const { announcementId } = useParams<{ announcementId: string }>(); // Get the announcement ID from the URL
 
   const { data: announcement, isLoading } = useAnnouncementById(announcementId!);
 
+  if (isLoading) {
+    return (
+      <Typography variant="h6" sx={{ textAlign: 'center', marginTop: MarginSize.large }}>
+        {t('loading')}
+      </Typography>
+    );
+  }
+
   return (
     <Box sx={{ minHeight: '100vh', p: 3 }}>
-      <ClassNavigationBar id={classId} className={className} />
-      {isLoading && (
-        <Typography variant="h6" sx={{ textAlign: 'center', marginTop: MarginSize.large }}>
-          {t('loading')}
-        </Typography>
-      )}
-
-      {!isLoading && (
-        <Box sx={{ width: '100%', maxWidth: { xs: '95%', sm: '90%' }, mx: 'auto', mt: 4, p: 2 }}>
-          <BackButton link={AppRoutes.classAnnouncements(classId)}></BackButton>
-          <Paper
-            sx={{
-              p: 2,
-              // maxWidth: { xs: '90%', sm: '90%' }, // Responsive width
-              mx: 'auto',
-              width: { xs: '90%', sm: '100%' },
-              mt: 1,
-            }}
-          >
-            <AnnouncementCard key={announcement?.id || 0} {...announcement!} />
-          </Paper>
-        </Box>
-      )}
+      <ClassNavigationBar id={announcement?.class.id || ''} className={announcement?.class.name} />
+      <Box sx={{ width: '100%', maxWidth: { xs: '95%', sm: '90%' }, mx: 'auto', mt: 4, p: 2 }}>
+        <BackButton link={AppRoutes.classAnnouncements(announcement?.class.id || '')}></BackButton>
+        <Paper
+          sx={{
+            p: 2,
+            // maxWidth: { xs: '90%', sm: '90%' }, // Responsive width
+            mx: 'auto',
+            width: { xs: '90%', sm: '100%' },
+            mt: 1,
+          }}
+        >
+          <AnnouncementCard key={announcement?.id || 0} {...announcement!} />
+        </Paper>
+      </Box>
     </Box>
   );
 }
