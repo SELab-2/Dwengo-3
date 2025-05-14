69e9166185f22475fab30fcd73caa48cdccf93e4
jasmaert <maertenjasper@gmail.com>
2025-05-14 15:29:56 +0200

    Assignment apiTests

--- test/vitest.config.api.ts
+++ test/vitest.config.api.ts
@@ -2,7 +2,7 @@ import { defineConfig } from 'vitest/config';
 
 export default defineConfig({
   test: {
-    include: ['./apiCalls/*.test.ts'],
+    include: ['./apiCalls/assignment.test.ts'],
     setupFiles: './apiCalls/mockApiClient.ts',
   },
 });
