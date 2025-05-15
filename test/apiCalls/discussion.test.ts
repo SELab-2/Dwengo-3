5e6ce06db1499bb41623ac8c6a36736414992dcb
jasmaert <maertenjasper@gmail.com>
2025-05-15 14:09:10 +0200

    Update discussion tests

--- test/apiCalls/discussion.test.ts
+++ test/apiCalls/discussion.test.ts
@@ -5,7 +5,9 @@ import {
   createDiscussion,
   fetchDiscussionById,
   fetchDiscussions,
+  updateDiscussion,
 } from '../../client/src/api/discussion';
+import { mock } from 'node:test';
 
 describe('Test discussionApi', () => {
   beforeEach(() => {
@@ -25,6 +27,27 @@ describe('Test discussionApi', () => {
     expect(response).toEqual(expected.data);
   });
 
+  test('Fetch discussions', async () => {
+    const expected = { data: 'Fetch discussions' };
+    (apiClient.get as any).mockResolvedValue(expected);
+    const userId = '123';
+    const assignmentId = '456';
+    const page = 1;
+    const pageSize = 10;
+
+    const response = await fetchDiscussions(userId, assignmentId, page, pageSize);
+
+    expect(apiClient.get).toHaveBeenCalledWith(ApiRoutes.discussion.list, {
+      params: {
+        userId,
+        assignmentId,
+        page,
+        pageSize,
+      },
+    });
+    expect(response).toEqual(expected.data);
+  });
+
   test('Fetch a discussion by its id', async () => {
     const expected = { data: 'Fetch discussion id' };
     (apiClient.get as any).mockResolvedValue(expected);
@@ -33,6 +56,22 @@ describe('Test discussionApi', () => {
     const response = await fetchDiscussionById(id);
 
     expect(apiClient.get).toHaveBeenCalledWith(ApiRoutes.discussion.get(id));
+    expect(response).toEqual(expected.data);
+  });
+
+  test('Update discussion', async () => {
+    const expected = { data: 'Update discussion' };
+    (apiClient.put as any).mockResolvedValue(expected);
+    const mockDiscussion = {
+      groupId: '1213',
+    };
+    const id = '456';
+
+    const response = await updateDiscussion(id, mockDiscussion);
+
+    expect(apiClient.put).toHaveBeenCalledWith(ApiRoutes.discussion.update(id), {
+      data: mockDiscussion,
+    });
     expect(response).toEqual(expected.data);
   });
 });
