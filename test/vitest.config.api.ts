668751c00f3ed18677839a53e366aa8f6d6e9058
jasmaert <maertenjasper@gmail.com>
2025-05-15 17:18:03 +0200

    LearningPathNode apiTest

--- test/vitest.config.api.ts
+++ test/vitest.config.api.ts
@@ -2,7 +2,7 @@ import { defineConfig } from 'vitest/config';
 
 export default defineConfig({
   test: {
-    include: ['./apiCalls/learningPath.test.ts'],
+    include: ['./apiCalls/learningPathNode.test.ts'],
     setupFiles: './apiCalls/mockApiClient.ts',
   },
 });
