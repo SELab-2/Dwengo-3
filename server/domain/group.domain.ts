9de38d797dcd4f375115d17d9b97a80059064b39
Robin Meersman <echteenrobin@gmail.com>
2025-05-16 23:52:46 +0200

    fix: conflicts resulted in new error numbers, updated domain layer to accomodate for this change

--- server/domain/group.domain.ts
+++ server/domain/group.domain.ts
@@ -12,10 +12,12 @@ export class GroupDomain {
     const group = await this.groupPersistence.getGroupById(id);
 
     if (group === null) {
-      throw new BadRequestError(40049);
+      throw new BadRequestError(40053);
     }
+
+    // -1 is seen as the "end" of the learning path
     if (group.currentNodeIndex > value && value !== -1) {
-      throw new BadRequestError(40048);
+      throw new BadRequestError(40052);
     }
     return await this.groupPersistence.updateCurrentNodeIndex(id, value);
   }
